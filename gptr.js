f24302ed25b911e65af0908708558bf9ab7feb1f
Robanik <168307797+Robanik@users.noreply.github.com>
2025-09-21 17:59:35 +0100

    Update gptr.js

--- gptr.js
+++ gptr.js
@@ -1,28 +1,41 @@
-function getBotResponse(userMessage, callback) {
-    const stages = [
-        "🤖 Читаю твоё сообщение...",
-        "🤖 Проверяю разрешения...",
-        "🤖 Думаю над ответом..."
-    ];
+// Ищем лучший ответ по ключевым словам
+function findAnswer(message) {
+  const words = message.toLowerCase().replace(/[^\w\sа-яё]/gi, '').split(/\s+/);
 
-    let stageIndex = 0;
+  let bestMatch = null;
+  let maxMatched = 0;
 
-    function nextStage() {
-        if (stageIndex < stages.length) {
-            callback(stages[stageIndex], false); // false = не финальный
-            stageIndex++;
-            setTimeout(nextStage, 1500);
-        } else {
-            let response = "🤖 Я пока не знаю ответа, но учусь!";
-            for (let item of knowledge) {
-                if (userMessage.toLowerCase().includes(item.question)) {
-                    response = item.answer;
-                    break;
-                }
-            }
-            callback(response, true); // true = финальный ответ
-        }
+  for (let item of knowledge) {
+    const matchedCount = item.keywords.filter(kw => words.includes(kw)).length;
+    if (matchedCount === item.keywords.length && matchedCount > maxMatched) {
+      bestMatch = item.answer;
+      maxMatched = matchedCount;
     }
+  }
 
-    nextStage();
+  return bestMatch || "🤖 Я пока не знаю ответа, но учусь!";
+}
+
+// Основная функция с этапами обдумывания
+function getBotResponse(userMessage, callback) {
+  const stages = [
+    "🤖 Читаю твоё сообщение...",
+    "🤖 Проверяю разрешения...",
+    "🤖 Думаю над ответом..."
+  ];
+
+  let stageIndex = 0;
+
+  function nextStage() {
+    if (stageIndex < stages.length) {
+      callback(stages[stageIndex], false);
+      stageIndex++;
+      setTimeout(nextStage, 1200); // 1.2 сек на этап
+    } else {
+      const response = findAnswer(userMessage);
+      callback(response, true);
+    }
+  }
+
+  nextStage();
 }
